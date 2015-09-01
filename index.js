d340a6db684bc5c9bd02ccb369d658cef65a0434
Jim Kang <jimkang@gmail.com>
2015-09-01 15:28:22 -0400

    Updated index.js to add a video element and wrap it in video.js, then play it.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var div = document.createElement('div');
-div.innerText = 'Oh hay you need to make an app here.'
-document.body.appendChild(div);
+var videojs = require('video.js');
+
+var video = document.createElement('video');
+video.id = 'presenter-video';
+video.src = 'https://da4w749qm6awt.cloudfront.net/engage-player/83ebde9d-0812-4071-8fb7-a97c1472ceb9/924a6001-506a-4af0-b8f3-cdc6873be638/presenter_trimmed.mp4?caches=0.942014982458204';
+video.type = 'video/mp4';
+document.body.appendChild(video);
+
+var videojsOpts = {
+
+};
+
+videojs(video, videojsOpts, init);
+
+function init() {
+  var player = this;
+  player.play();
+}
