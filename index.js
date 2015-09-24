23e36f4002ff4ae36f3f195642d9a694b663afcd
Jim Kang <jimkang@gmail.com>
2015-09-24 15:51:04 -0400

    Scrubbed out video.js momentarily.

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-var videojs = require('video.js');
 var renderVideo = require('./render-video');
-
-var videojs = require('video.js');
-var renderVideo = require('./render-video');
-
-function videoRendered() {
-  var player = this;
-  // player.play();
-}
-
-function videoRendered2() {
-  var player = this;
-}
 
 renderVideo(
   {
     url: 'https://da4w749qm6awt.cloudfront.net/engage-player/83ebde9d-0812-4071-8fb7-a97c1472ceb9/924a6001-506a-4af0-b8f3-cdc6873be638/presenter_trimmed.mp4?caches=0.942014982458204',
     id: 'presenter-video',
-    parent: document.querySelector('#video-root-1'),
+    parent: document.querySelector('#video-container'),
     progressControlTop: 450
-  },
-  videoRendered
+  }
 );
 
 renderVideo(
   {
     url: 'https://da4w749qm6awt.cloudfront.net/engage-player/83ebde9d-0812-4071-8fb7-a97c1472ceb9/d7086069-ec1f-4862-a75f-822cbffdee5f/presentation_trimmed.mp4',
     id: 'presentation-video',
-    parent: document.querySelector('#video-root-2'),
+    parent: document.querySelector('#video-container'),
     progressControlTop: 900
-  },
-  videoRendered2
+  }
 );
--- render-video.js
+++ render-video.js
@@ -1,6 +1,3 @@
-var videojs = require('video.js');
-import ProgressControl from '../video.js/src/js/control-bar/progress-control/progress-control.js';
-
 function renderVideo(opts, done) {
   var {
     id,
@@ -13,79 +10,43 @@ function renderVideo(opts, done) {
   video.id = id;
   video.src = url;
   video.type = 'video/mp4';
-  parent.classList.add('vjs-default-skin');
+  // parent.classList.add('vjs-default-skin');
   parent.appendChild(video);
 
-  var videojsOpts = {
-    controls: {
-      playToggle: {},
-      fullscreenToggle: {},
-      currentTimeDisplay: {},
-      timeDivider: {},
-      durationDisplay: {},
-      remainingTimeDisplay: {},
-      progressControl: {},
-      volumeControl: {},
-      muteToggle: {}
-    },
-    textTrackDisplay: false,
-    textTrackSettings: false,
-    autoplay: false,
-    preload: 'auto',
-    // poster: TODO,
-    loop: false,
-    width: 784,
-    height: 441,
-    controlBar: false
-  };
+  // var videojsOpts = {
+  //   controls: {
+  //     playToggle: {},
+  //     fullscreenToggle: {},
+  //     currentTimeDisplay: {},
+  //     timeDivider: {},
+  //     durationDisplay: {},
+  //     remainingTimeDisplay: {},
+  //     progressControl: {},
+  //     volumeControl: {},
+  //     muteToggle: {}
+  //   },
+  //   textTrackDisplay: false,
+  //   textTrackSettings: false,
+  //   autoplay: false,
+  //   preload: 'auto',
+  //   // poster: TODO,
+  //   loop: false,
+  //   width: 784,
+  //   height: 441,
+  //   controlBar: false
+  // };
 
-  videojs(video, videojsOpts, setUpControls);
+  // videojs(video, videojsOpts, setUpControls);
 
-  function setUpControls() {
-    var player = this;
-    var progressControl = new ProgressControl(player);
-    var pcEl = progressControl.el();
-    pcEl.style.top = progressControlTop;
-    parent.appendChild(pcEl);
-    done(player);
-  }
-  // var progressControl = new ProgressControl(createMockPlayer());
-  // var pcEl = progressControl.el();
-  // pcEl.style.height = 64;
-  // video.appendChild(pcEl);
+  // function setUpControls() {
+  //   var player = this;
+  //   var progressControl = new ProgressControl(player);
+  //   var pcEl = progressControl.el();
+  //   pcEl.style.top = progressControlTop;
+  //   parent.appendChild(pcEl);
+  //   done(player);
+  // }
 
-}
-
-
-function noop() {
-}
-
-function createMockPlayer() {
-  var isScrubbing = false;
-
-  function scrubbing(val) {
-    if (val === undefined) {
-      return isScrubbing;
-    }
-    else {
-      isScrubbing = val;
-    }
-  }
-
-  return {
-    id: noop,
-    on: noop,
-    ready: noop,
-    tech_: {
-      'featuresVolumeControl': false
-    },
-    volume: noop,
-    muted: noop,
-    reportUserActivity: noop,
-    duration: () => 10000,
-    scrubbing: scrubbing,
-    currentTime: () => 5000
-  };
 }
 
 module.exports = renderVideo;
